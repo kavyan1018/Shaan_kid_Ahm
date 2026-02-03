e72e72ba01c98cca9b516bc6be9ebe3d75a2fa13
Kavyan Nayak <kavyannayak104@gmail.com>
2026-02-03 16:56:48 +0530

    Varibles and op

---
+++ Js/varibles.js
@@ -0,0 +1,39 @@
+// c -> int, char, float 
+
+
+// js -> var, let, const 
+
+/*
+        var ->  old
+        let -> Modern  -> value can changes  
+        const -> Fixed value -> value cannot changes
+*/
+
+let nam = "Dk";  // string
+let marks = 90;  // int / Numebr 
+let pass = true; // boolean
+
+/*
+
+        user input :
+                prompt("Enter your name : ")
+
+
+
+        op : 
+            Arithmetic : + - * / %
+            Assignment : =  +=  -=  *=  /=
+            Comparison :  ==  ===  !=  !==  >  <  >=  <=
+            Logical :  &&  ||  !
+            
+            if - else , switch
+            loops : for , while , do while
+
+        printf("Name is : %s", nam);
+
+*/
+
+
+console.log("Name is : " + nam);        
+console.log("Marks is : " + marks);        
+console.log("Pass status is : " + pass);        
